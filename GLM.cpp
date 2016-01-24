315bf09671494ec1649c63eaac4b97ca2e543228
Joao Neves <joaoneves792@gmail.com>
2016-01-24 22:50:05 +0000

    Created the GLM class for helping with matrix operations

---
+++ GLM.cpp
@@ -0,0 +1,75 @@
+#include "ms3d.h"
+#include <GLES3/gl3.h>
+
+#define MODEL 1
+#define VIEW 2
+#define PROJECTION 3
+
+GLM::GLM(Shader* shader){
+	_shader = shader;
+
+	_Model = glm::mat4(1.0f);
+	_View = glm::mat4(1.0f);
+	_Projection = glm::mat4(1.0f);
+	
+	
+	_modelID = glGetUniformLocation( _shader->getShader(), "Model" );
+	_viewID = glGetUniformLocation( _shader->getShader(), "View" );
+	_projectionID = glGetUniformLocation( _shader->getShader(), "Projection");
+
+	_activeMatrix = &_Projection;
+}
+
+GLM::~GLM(){
+	//Maybe do something here?
+}
+
+void GLM::selectMatrix(int Matrix){
+	if(Matrix == MODEL)
+		_activeMatrix = &_Model;
+	else if(Matrix == VIEW)
+		_activeMatrix = &_View;
+	else if(Matrix == PROJECTION)
+		_activeMatrix = &_Projection;
+	else
+		return;
+	_activeMatrixID = Matrix;
+}
+
+void GLM::perspective(double fov_degrees, double aspect_ratio, double near, double far){
+	_Projection = glm::perspective(glm::radians(fov_degrees), aspect_ratio, near, far);
+	glUniformMatrix4fv(_projectionID, 1, GL_FALSE, glm::value_ptr(_Projection));
+}
+
+void GLM::loadIdentity(){
+	*(_activeMatrix) = glm::mat4(1.0f);
+	uploadMatrix();
+}
+
+void GLM::translate(double x, double y, double z){
+	*(_activeMatrix) = glm::translate(*(_activeMatrix), glm::vec3(x, y, z));
+	uploadMatrix();
+}
+void GLM::rotate(float angle, double x, double y, double z){
+	*(_activeMatrix) = glm::rotate(*(_activeMatrix), angle, glm::vec3(x, y, z));
+	uploadMatrix();
+}
+
+void GLM::pushMatrix(){
+	_stack.push(*(_activeMatrix));
+}
+
+void GLM::popMatrix(){
+	*(_activeMatrix) = _stack.top();
+	_stack.pop();
+	uploadMatrix();
+}
+
+void GLM::uploadMatrix(){
+	if(_activeMatrixID == MODEL)
+		glUniformMatrix4fv(_modelID, 1, GL_FALSE, glm::value_ptr(_Model));
+	else if(_activeMatrixID == VIEW)
+		glUniformMatrix4fv(_viewID, 1, GL_FALSE, glm::value_ptr(_View));
+	else if(_activeMatrixID == PROJECTION)
+		glUniformMatrix4fv(_projectionID, 1, GL_FALSE, glm::value_ptr(_Projection));
+}
--- ms3d.h
+++ ms3d.h
@@ -1,10 +1,14 @@
 #ifndef _ms3d_H_
 #define _ms3d_H_
 
+#include <stack>
 #include <ms3d/MS3DFile.h>
 #include <ms3d/Textures.h>
 #include <ms3d/Shader.h>
 #include <GL/glut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class shader{
 private:
@@ -36,4 +40,38 @@ public:
 	Tex(char* filename);
 	int getTexture();
 };
+
+class GLM{
+private:
+	Shader* _shader;
+
+	glm::mat4 _Model;
+	glm::mat4 _View;
+	glm::mat4 _Projection;
+
+	glm::mat4* _activeMatrix;
+	int _activeMatrixID;
+
+	GLuint _modelID;
+	GLuint _viewID;
+	GLuint _projectionID;
+
+	std::stack<glm::mat4> _stack;
+
+public:
+	GLM(Shader* shader);
+	virtual ~GLM();
+
+	void selectMatrix(int Matrix);
+	void perspective(double fov_degrees, double aspect_ratio, double near, double far);
+	//void otho(...)
+	void loadIdentity();
+	void translate(double x, double y, double z);
+	void rotate(float angle, double x, double y, double z);
+	void pushMatrix();
+	void popMatrix();
+private:
+	void uploadMatrix();
+};
+
 #endif
